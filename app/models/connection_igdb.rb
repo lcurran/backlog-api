dd85e3ac6647173ae1b5562d249a8e9815ba36d6
lcurran <lcurran327@gmail.com>
2016-07-29 13:46:29 -0400

    Has IGDB module, added figaro and unirest to gems
    IGDB module calls get to IGDB/games database on initialize and returns status code
    & parsed json body

---
+++ app/models/connection_igdb.rb
@@ -0,0 +1,16 @@
+require 'unirest'
+
+# IGDD API GAME SEARCH CONNECTION
+class ConnectionIGDB
+  def initialize(params)
+    response = Unirest.get 'https://igdbcom-internet-game-database-v1.p.mashape.com/games/',
+                           headers: {
+                             'X-Mashape-Key' => ENV["igdb_api_key"],
+                             'Accept' => 'application/json'
+                           },
+                           parameters: params
+
+    puts response.code
+    puts response.body
+  end
+end
